99d7d65d0b8c79897e3deb818d34ca97b2b2a42e
Tatiana Vakaruk <tatiana.vakaruk1@gmail.com>
2026-08-29 13:36:24 +0300

    Добавил новый файл

---
+++ lesson21/task4/index.js
@@ -0,0 +1,20 @@
+const getTitle = () => {
+    const title = document.querySelector('.title');
+    return title.textContent;
+}
+const getDescription = () => {
+    const description = document.querySelector('.about');
+    return description.innerText;
+}
+const getPlans = () => {
+    const plans = document.querySelector('.plans');
+    return plans.innerHTML;
+}
+const getGoal = () => {
+    const goal = document.querySelector('.goal');
+    return goal.outerHTML;
+}
+console.log(getTitle());
+console.log(getDescription());
+console.log(getPlans());
+console.log(getGoal());
