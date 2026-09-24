bc00cd8251659415838697c8845fa37e30a48b41
Tatiana Vakaruk <tatiana.vakaruk1@gmail.com>
2026-09-24 20:41:07 +0300

    описание изменений

--- lesson29/task5/index.js
+++ lesson29/task5/index.js
@@ -1,30 +1,16 @@
-export const addImageV2 = url => {
-  // put your code here
-  const p = new Promise((resolve, reject) => { 
-    const img = document.createElement('img');
-  img.setAttribute('alt', 'User avatar');
-  img.src = url;
+import { addImage } from "./addImage.js";
 
-  const pageElem = document.querySelector('.page');
-  pageElem.append(img);
-
-  const onImageLoaded = () => {
-    const { width, height } = img;
-    resolve({ width, height });
-  };
-
-  const onImageLoadError = () => reject(new Error ('Image load failed'));
-
-  img.addEventListener('load', onImageLoaded);
-
-  img.addEventListener('error', onImageLoadError);
+export const addImageV2 = (url) => {
+  return new Promise((resolve, reject) => {
+    addImage(url, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
   });
-  return p;
 };
-
-// examples
-
-addImageV2('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg')
-  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch(error => console.log(error)); // ==> 'Image load failed'
-
+addImageV2("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg")
+  .then((data) => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch((error) => console.log(error)); // ==> 'Image load failed'
